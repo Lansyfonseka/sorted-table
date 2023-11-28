8d9e771f6509e5d1caa014e63882c4f72d2329a5
lansyfonseka <twinkstriker@gmail.com>
2023-11-28 23:20:53 +0300

    add style and start sorting

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,34 @@
 <script setup>
-import TheWelcome from './components/TheWelcome.vue'
 import Table from './components/Table/Table.vue'
+import database from './database/data.json';
+import { ref } from 'vue'
+
+const databaseRework = ref( database.map( client => {
+  client.dates = client.dates.map( date => {
+    date.start_date  = new Date(date.start_date);
+    date.end_date = new Date(date.end_date);
+    return date
+  });
+  client.status = client.dates.map ( date => {
+    let nowToEnd = Math.floor( (new Date() - date.end_date)/86400000 );
+    let nowToStart = Math.floor( (new Date() - date.start_date)/86400000 );
+    return {
+      done: nowToEnd > 0,
+      days: nowToStart > 0 ? nowToEnd : -nowToStart
+    }    
+  })
+  return client;
+}))
+
+function test () {
+  databaseRework.value.sort( (a,b) => a.o_id - b.o_id);
+}
+
+
 </script>
 
 <template>
     <!-- <TheWelcome /> -->
-    <Table></Table>
+    <Table :databaseOfClients = databaseRework @sortID="test"></Table>
 </template>
 
--- src/components/Table/Table.vue
+++ src/components/Table/Table.vue
@@ -1,14 +1,19 @@
 <script setup>
-import database from '../../database/data.json';
 import './Table.scss';
 
-console.log(database);
+const props = defineProps({
+  databaseOfClients: Array
+});
+
+const emits = defineEmits(['sortID']);
+console.log(props.databaseOfClients);
+
 </script>
 
 <template>
   <div class="table">
     <div class="table-row table-header">
-      <div>№ Клиента</div>
+      <div @click="$emit('sortID')">№ Клиента</div>
       <div>Имя</div>
       <div>Диета</div>
       <div>Тариф</div>
@@ -19,25 +24,39 @@ console.log(database);
       <div>Оплата</div>
       <div>Комментарий курьера</div>
       <div>Комментарий внутренний</div>
-      <div>Срок исполнения</div>
+      <div>Статус</div>
     </div>
-    <div v-for="client in database" class="table-row">
+    <div v-for="client in databaseOfClients" class="table-row">
       <div>{{ client.o_id }}</div>
       <div>{{ client.client_name }}</div>
-      <div class="table-row--listItem itemCenter">
+      <div class="table-row--listItem table-row--listItemDotted">
         <div v-for="diet in client.diets">{{ diet }}</div>
       </div>
-      <div class="table-row--listItem">
+      <div class="table-row--listItem table-row--listItemDotted itemCenter">
         <div v-for="tarif in client.tariff">{{ tarif }}</div>
       </div>
       <div>{{ client.address }}</div>
       <div>{{ client.phone }}</div>
-      <div></div>
-      <div>{{ client.discount }}</div>
-      <div>{{ client.order_sum }}</div>
+      <div class="table-row--listItem table-row--listItemDotted itemCenter">
+        <div v-for="date in client.dates">{{ date.start_date.toLocaleDateString() }} - {{ date.end_date.toLocaleDateString() }}</div>
+      </div>
+      <div class="itemCenter">{{ client.discount }}</div>
+      <div class="table-row--listItem">
+        <div>Стоим:{{ client.order_sum }}</div>
+        <div>{{ client.pay_status }}</div>
+        <div>Долг:{{ client.order_sum - client.order_payed }}</div>
+      </div>
       <div>{{ client.courier_comment }}</div>
       <div>{{ client.inner_comment }}</div>
-      <div>1</div>
+      <div class="table-row--listItem table-row--listItemDotted">
+        <div v-for="timeStatus in client.status">
+          {{ 
+            timeStatus.done ? `Завершилось ${timeStatus.days} дней назад` : 
+            timeStatus.days < 0 ? `Завершиться через ${-timeStatus.days} дней` : 
+            `Начнется через ${timeStatus.days} дней`
+          }}
+        </div>
+      </div>
     </div>
   </div>
 </template>
